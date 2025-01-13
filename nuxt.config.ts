63bf780c53a1990b43fdfeb06bffc0c8909f69dd
developer05 <longlundy@ocs.local>
2025-01-13 12:06:39 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,54 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  devServer: {
+    port: 3000,
+  },
+  app: {
+    rootId: 'nuxt-ui',
+    rootTag: 'nuxt-ui',
+    baseURL: '/',
+    buildAssetsDir: '_oc',
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      bodyAttrs: {
+        class: 'oc-body',
+      },
+      meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+      link: [],
+      script: [],
+    },
+  },
+
+  components: true,
+  css: [
+    '~/assets/css/tailwind.css',
+    '~/assets/css/main.css',
+  ],
+  postcss: {
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    esbuild: {
+      drop: ['console', 'debugger'],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: '@use "@/assets/styles/_vars.scss" as *;',
+        },
+      },
+    },
+    optimizeDeps: {
+      exclude: ['#import'], // Exclude this module from Vite's optimization process
+    },
+  },
+  modules: ['@nuxt/ui'],
 })
